3eac5726661292f49f533b07ca24c1a95a2ef7ae
otapi0 <otapi0@gmail.com>
2017-08-09 01:06:46 +0200

    fff

--- app/src/main/assets/RobotCarPilot/RobotCarPilot.ino
+++ app/src/main/assets/RobotCarPilot/RobotCarPilot.ino
@@ -9,13 +9,31 @@ int inputPin = 4;    // define pin for sensor echo
 int outputPin = 5;   // define pin for sensor trig
 
 Servo myservo;        // set myservo
-int isTurning = 0;
-int isRunning = 0;
 
 elapsedMillis timeElapsed; 
 
-int distance()   // measure the distance ahead
+void setup()
 {
+  Serial.begin(9600);     // define pin for motor output
+
+  //Serial.write("Power On");
+  
+  pinMode(pinLB, OUTPUT); // pin 12
+  pinMode(pinLF, OUTPUT); // pin 3 (PWM)
+  pinMode(pinRB, OUTPUT); // pin 13
+  pinMode(pinRF, OUTPUT); // pin 11 (PWM)
+  pinMode(inputPin, INPUT);    // define input pin for sensor
+  pinMode(outputPin, OUTPUT);  // define output pin for sensor
+
+  // attaches the servo on pin 9 to the servo object 
+  myservo.attach(9);
+  myservo.write(90);
+  timeElapsed = 0;
+}
+
+int distance()   // measure the distance ahead (cm)
+{
+  
   digitalWrite(outputPin, LOW);   // ultrasonic sensor transmit low level signal 2μs
   delayMicroseconds(2);
   digitalWrite(outputPin, HIGH);  // ultrasonic sensor transmit high level signal10μs, at least 10μs
@@ -26,133 +44,61 @@ int distance()   // measure the distance ahead
   return Fdistance;              // read the distance into Fdist
 }
 
-void advance()     // move forward
-{
-  digitalWrite(pinLB, LOW);   // right wheel moves forward
-  digitalWrite(pinRB, HIGH);  // left wheel moves forward
-  analogWrite(pinLF, 255);
-  analogWrite(pinRF, 255);
-}
-void stopp()         // stop
-{
-  digitalWrite(pinLB, HIGH);
-  digitalWrite(pinRB, LOW);
-  analogWrite(pinLF, 0);
-  analogWrite(pinRF, 0);
-}
-void right()        // turn right (single wheel)
-{
-  digitalWrite(pinLB, LOW); // left wheel moves forward
-  digitalWrite(pinRB, LOW); // right wheel moves backward
-  analogWrite(pinLF, 200);
-  analogWrite(pinRF, 200);
-}
-void left()         // turn left (single wheel)
-{
-  digitalWrite(pinLB, HIGH); // left wheel moves forward
-  digitalWrite(pinRB, HIGH); // right wheel moves backward
-  analogWrite(pinLF, 200);
-  analogWrite(pinRF, 200);
-}
-void back()          // move backward
-{ digitalWrite(pinLB, HIGH); // motor moves to left rear
-  digitalWrite(pinRB, LOW); // motor moves to right rear
-  analogWrite(pinLF, 200);
-  analogWrite(pinRF, 200);
+char inData[20]; // Allocate some space for the string
+char inChar=-1; // Where to store the character read
+byte index = 0; // Index into array; where to store the character
+
+void setup() {
+
 }
 
-void setup()
-{
-  Serial.begin(9600);     // define pin for motor output
-  pinMode(pinLB, OUTPUT); // pin 12
-  pinMode(pinLF, OUTPUT); // pin 3 (PWM)
-  pinMode(pinRB, OUTPUT); // pin 13
-  pinMode(pinRF, OUTPUT); // pin 11 (PWM)
-  pinMode(inputPin, INPUT);    // define input pin for sensor
-  pinMode(outputPin, OUTPUT);  // define output pin for sensor
+void command(string cmd) {
+  switch (cmd[0]) {
+            case 'l':
+              digitalWrite(pinLB, HIGH);
+              analogWrite(pinLF, cmd[1]);
+              command(cmd.substring(2);
+              break;
+            case 'L':
+              digitalWrite(pinLB, LOW);
+              analogWrite(pinLF, cmd[1]);
+              command(cmd.substring(2);
+              break;
+            case 'r':
+              digitalWrite(pinLB, LOW);
+              analogWrite(pinLF, cmd[1]);
 
-    // attaches the servo on pin 9 to the servo object 
-  myservo.attach(9);
-  myservo.write(90);
-  timeElapsed = 0;
+              // TODO: combine two bytes to a short: http://projectsfromtech.blogspot.hu/2013/09/combine-2-bytes-into-int-on-arduino.html
+              short timer = cmd[2]+cmd[4];
+              break;
+            case 'R':
+              digitalWrite(pinLB, HIGH);
+              analogWrite(pinLF, cmd[1]);
+              command(cmd.substring(2);
+              break;
+            
+          }
 }
 
 void loop()
 {
-  /*
-  if (timeElapsed > 100 && !isTurning && isRunning) 
-  {       
-    int dist = distance();
-    Serial.println(dist);
-    if (dist < 20) {
-      stopp();
-      isTurning = 0;
-      isRunning = 0;
-      myservo.write(20);
-      delay(500);
-      myservo.write(160);
-      delay(500);
-      myservo.write(90);
-    }
-    timeElapsed = 0;       // reset the counter to 0 so the counting starts over...
-  }
-*/
-    int val = Serial.read();
-    switch (val) {
-      case 'U':
-        myservo.write(90);
-        /*
-        if (distance() < 20) {
-          stopp();
-          isTurning = 0;
-          myservo.write(20);
-          delay(500);
-          myservo.write(160);
-          delay(500);
-          myservo.write(90);
+    while (Serial.available() > 0) // Don't read unless
+                                   // there you know there is data
+    {
+        inChar = Serial.read(); // Read a character
+        inData[index] = inChar; // Store it
+        index++; // Increment where to write next
+        if (inChar == '\n') {
+          inData[index] = '\0'; // Null terminate the string
+          index = 0;
+          //TODO: do the command
+          command(inData);
+          
+        }
+        if (index>10) {
+          // Command should be shorter - error!
           break;
-        }*/
-        advance();
-        isRunning = 1;
-        delay(300);
-        stopp();
-        isTurning = 0;
-        
-        break;
-      case 'D':
-        back();
-        myservo.write(90);
-        delay(300);
-        stopp();
-        isTurning = 0;
-        isRunning = 0;
-        break;
-      case 'L':
-        left();
-        myservo.write(130);
-        isTurning = 1;
-        isRunning = 0;
-        delay(100);
-        stopp();
-        isTurning = 0;
-        break;
-      case 'R':
-        myservo.write(50);
-        right();
-        isTurning = 1;
-        isRunning = 0;
-        delay(100);
-        stopp();
-        isTurning = 0;
-        
-        break;
-      case 'S':
-        myservo.write(90);
-        stopp();
-        isTurning = 0;
-        isRunning = 0;
-        break;
-      default:
-         break;
+        }
     }
 }
+
--- app/src/main/java/com/otapigems/robotcar/RobotCommander.java
+++ app/src/main/java/com/otapigems/robotcar/RobotCommander.java
@@ -9,58 +9,97 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 /**
  * Created by otapi_aocivry on 2017. 08. 06..
  */
 
 public class RobotCommander {
-    class RobotCommAsk {
-        /**
-         * Alert back asynch if ultrasonic sensor sees something at this distance (cm) or closer. To disable, set it to 0 (by default)
-         */
-        public float alertDistance = 0;
-        /**
-         * Stop motors if something is ahead or closer (in cm). Default is 1. To disable, set this to -1
-         */
-        public float stopDistance = 5;
-        /**
-         * Answer back the measured distance ahead (in cm)
-         */
-        public boolean tellDistance = false;
-        /**
-         * Move right motor by speed between -255 and 255.
-         */
-        public int motorRight = 0;
-        /**
-         * Move right motor by speed between -255 and 255
-         */
-        public int motorLeft = 0;
-        /**
-         * Stop motors when this time (milliseconds) runs out. Default is 300
-         */
-        public int motorTimer = 300;
-        /**
-         * Set the neckPosition. 0=left, 90 ahead (default), 180=right
-         */
-        public int neckPosition = 90;
+    private byte intToUnsignedByte(int in) {
+        return (byte) (in-Integer.MAX_VALUE);
     }
-    class RobotCommAnswer {
-        /**
-         * Measured distance ahead (cm)
-         */
-        public float distance;
-        /**
-         * Motors stopped (upon stopDistance)
-         */
-        public boolean motorsStopped = false;
-        /**
-         * Any message
-         */
-        public String message = null;
+    /**
+     * Alert back asynch if ultrasonic sensor sees something at this distance (cm) or closer. To disable, set it to 0 (by default)
+     * @param distance in cm
+     */
+    public void cmdAlertDistance(int distance) {
+        //// TODO: 2017. 08. 08. Implement this...
     }
 
-    public RobotCommAsk robotCommAsk;
+    /**
+     * Stop motors if something is ahead or closer (in cm). Default is 5. To disable, set this to 0
+     * @param distance 0..255
+     */
+    public void cmdStopDistance(int distance) {
+        if (distance<0 || distance > 255) {
+            throw new IndexOutOfBoundsException("Distance should be between 0..255");
+        }
+
+        sendToRobot("d"+intToUnsignedByte(distance));
+    }
+
+    /**
+     * Answer back the measured distance ahead
+     * @return obstacle distance in cm
+     */
+    public byte cmdTellDistance() {
+        /// TODO: 2017. 08. 08. Implement this...
+        return 0;
+    }
+
+    /**
+     * Run the motors. Negative is backward, 0 is stop
+     * @param left -255...255
+     * @param right -255...255
+     * @param timer Stop motors when this time (milliseconds) runs out. Default is 300, 0..32,767
+     */
+    public void cmdMotor(int left, int right, int timer) {
+
+        if (Math.abs(left)>255 || Math.abs(right)>255) {
+            throw new IndexOutOfBoundsException("Motor parameters are out of bounds. Should be below 256.");
+        }
+        if (timer<0 || timer>32767) {
+            throw new IndexOutOfBoundsException("Motor timer is out of bounds. Should be between 0..32,767.");
+        }
+        String command = "";
+        if (left < 0) {
+            command+= "l";
+        } else {
+            command+="L";
+        }
+        command+=intToUnsignedByte(Math.abs(left));
+
+        if (right < 0) {
+            command+= "r";
+        } else {
+            command+="R";
+        }
+        command+=intToUnsignedByte(Math.abs(right));
+
+        short unsignedTimer = (short) ((short)timer- Short.MAX_VALUE);
+        command+=ByteBuffer.allocate(2).putShort(unsignedTimer).array();
+        sendToRobot(command);
+    }
+
+    /**
+     * Set neck position. 0 is left, 90 is ahead, 180 is right
+     * @param position degrees
+     */
+    public void cmdNeckPosition(int position) {
+        if (position<0 || position > 180) {
+            throw new IndexOutOfBoundsException("Neck position should between 0..180");
+        }
+        sendToRobot("n"+intToUnsignedByte(position));
+    }
+
+    public void reset() {
+        cmdAlertDistance(0);
+        cmdStopDistance(5);
+        cmdMotor(0,0,1);
+        cmdNeckPosition(90);
+    }
+
     private BluetoothSocket robotSocket;
     private OutputStream outputStream;
     private InputStream inputStream;
@@ -71,16 +110,11 @@ public class RobotCommander {
         this.robotSocket = robotSocket;
         this.outputStream = outputStream;
         this.inputStream = inputStream;
-        robotCommAsk = new RobotCommAsk();
-        sendToRobot();
+        reset();
     }
 
     public void forward() {
-        robotCommAsk.motorLeft = 255;
-        robotCommAsk.motorRight = 255;
-        sendToRobot();
-        robotCommAsk.motorLeft = 0;
-        robotCommAsk.motorRight = 0;
+        cmdMotor(255,255,300);
     }
 
     void print(String message)
@@ -88,13 +122,10 @@ public class RobotCommander {
         Log.d("RobotCommander", message);
     }
 
-    public void sendToRobot() {
-        Gson gson = new Gson();
-        String json = gson.toJson(robotCommAsk);
-
-        //json.concat("\n");
+    public void sendToRobot(String message) {
+        message.concat("\n");
         try {
-            outputStream.write(json.getBytes());
+            outputStream.write(message.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -146,6 +177,7 @@ public class RobotCommander {
     */
 
     public void close() throws IOException {
+        reset();
         stopThread = true;
         outputStream.close();
         inputStream.close();
