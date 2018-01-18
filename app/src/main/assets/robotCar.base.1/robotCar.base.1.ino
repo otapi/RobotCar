d968c39d84179eed06e1bada24c87973ac0ecd12
otapi <otapi@protonmail.ch>
2018-01-18 22:46:37 +0100

    License added

--- app/src/main/assets/robotCar.base.1/robotCar.base.1.ino
+++ app/src/main/assets/robotCar.base.1/robotCar.base.1.ino
@@ -1,3 +1,23 @@
+/*
+    robotCar.base
+    Ardunio Controller firmware to control a rolling robot.
+
+    Copyright (C) 2018  Barnabás Nagy - otapiGems.com - otapiGems@protonmail.ch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 #include <Servo.h>
 #include <elapsedMillis.h>
 
